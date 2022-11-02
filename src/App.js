0ae9436e12a60f627e649bad908e0136e8709c32
danielflorencio <danielflorencio20@gmail.com>
2022-11-02 10:29:23 -0300

    added the services section

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ImgTitleDescriptionSection from './components/ImgTitleDescriptionSection'
 import Title from './components/Title';
 import SubTitle from './components/Subtitle';
 import IconsAndDescriptionSection from './components/IconsAndDescriptionSection';
-// import IconAndDescriptionBlock from './components/IconAndDescriptionBlock';
+import RedCTAButton from './components/RedCTAButton';
 function App() {
   return (
     <>
@@ -27,10 +27,10 @@ function App() {
       Wether you use one or the other, just make sure you provide the component with an img, a title, a description and a style.
       */}
 
-      <ImgTitleDescriptionSection img="" subtitle="title" description="description" style="left-text-right-img" />
-      <ImgTitleDescriptionSection img="" subtitle="title" description="description" style="right-text-left-img" />
-      <ImgTitleDescriptionSection img="" subtitle="title" description="description" style="left-text-right-img" />
-      <ImgTitleDescriptionSection img="" subtitle="title" description="description" style="right-text-left-img" /> 
+      <ImgTitleDescriptionSection img="" subtitle="Google Ads" description="Have you ever wondered why the patient went to your competitor and not you? With the power of Google Ads, you will be found by up to 3x more patients! Be the one who is ahead of the competition." style="left-text-right-img" />
+      <ImgTitleDescriptionSection img="" subtitle="Attractive and persuasive website" description="We will create the perfect website to make your customer delighted with your service. With this, your patient will get to your attendant more prepared to schedule for a private consultation." style="right-text-left-img" />
+      <ImgTitleDescriptionSection img="" subtitle="Metric analysis and improving" description="Long term is the key! In addition to bringing you many new patients from the first month onwards, with our quarterly analysis of metrics and structure, the trend is that your result will always increase each month." style="left-text-right-img" />
+      <ImgTitleDescriptionSection img="" subtitle="Facebook and Instagram ads" description="Keep actively selling your best procedures to the world’s most used social networks. With Facebook and Instagram working for you, getting your treatments and procedures to reach more people will be easier than taking a nap in a rainy afternoon." style="right-text-left-img" /> 
       
       {/*// <Title title=''/>*/}
         {/* <IconBlock icon="" title="title" description="description" cta="cta"/> */}
@@ -38,7 +38,18 @@ function App() {
 
       {/* <ImgTitleDescriptionSection img="" subtitle="title" description="description" style="left-text-right-img" /> */}
 
-      {/*<Title title=''/>
+      <ImgTitleDescriptionSection img="" subtitle="Step 1 - Free consultancy." description="Here in this step we will analyze everything that is currently being done in your company and design which marketing strategy will make the most sense for you at the moment. Just this meeting will be already like a free mentorship for you." style="left-text-right-img" />
+      <ImgTitleDescriptionSection img="" subtitle="Step 2 - Execution and revenue." description="If the contract is closed, here is where we will make your company start receiving more patients. We will collect the necessary data to create your persuasive website, create your ad campaigns, and increase your number of consultations and procedures." style="right-text-left-img" />
+      <ImgTitleDescriptionSection img="" subtitle="Step 3 - Analysis and scale." description="Here is where happiness lives, our favorite step. After collecting the data and improving the first campaigns, we will be able to know exactly what to do to further multiply your profit and the arrival of new patients." style="left-text-right-img" />
+
+      
+      {/* <Title title='' background_color={}/> */}
+      <RedCTAButton cta='Schedule meeting'/>
+      
+      
+      
+      {/*
+      
       <ImgTitleDescriptionSection img="" title="title" description="description" style="left-text-right-img" />
       <ImgTitleDescriptionSection img="" title="title" description="description" style="right-text-left-img" />
       <ImgTitleDescriptionSection img="" title="title" description="description" style="left-text-right-img" />
