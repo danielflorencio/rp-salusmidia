3bb9dbbedb61350ef1b0231d2f2747a656546d2e
danielflorencio <danielflorencio20@gmail.com>
2022-11-03 07:58:10 -0300

    styles the FAQ section

--- src/components/Faqitem.js
+++ src/components/Faqitem.js
@@ -1,8 +1,40 @@
+import './component-styles.css'
+import { useState } from 'react'
+import { isVisible } from '@testing-library/user-event/dist/utils'
+
+function Answer({Answer, isVisible}){
+    if(isVisible){
+        return(
+            <div className='answer'>{Answer}</div>
+        )
+    } else{
+        return null
+    }
+}
 export default function FaqItem({question, answer}){
+    const [visibility, setVisibility] = useState(true)
+    function changeVisibility(visibility){
+        if (visibility === true){
+            setVisibility(false)
+        } else{
+            setVisibility(true)
+        }
+    }
     return(
-        <div>
-            <div className="question">{question}</div>
-            <div className="answer">{answer}</div>
+        <div className="accordion-item">
+
+            <div className="question"><a class="accordion-link" onClick={changeVisibility(visibility)}>{question}</a></div>
+            <Answer Answer={answer} isVisible={visibility}/>
+            {/* {visibility ? <div className='answer'></div> : <div></div> } */}
+            {/* // <div className="answer">{answer}</div> */}
         </div>
     )
 }
+
+{/* <div class="accordion-item" id="question-1">
+<div>
+<a class="accordion-link" href="#question-1">
+    Question 1
+    <ion-icon name="add-outline" class="add-icon"></ion-icon>
+    <ion-icon name="remove-outline" class="remove-icon"></ion-icon>
+</a> */}
--- src/components/Faqsection.js
+++ src/components/Faqsection.js
@@ -1,12 +1,19 @@
 import './component-styles.css';
 import FaqItem from './FaqItem'
+import Title from './Title';
 export default function FaqSection(){
     return(
         <>
-            <div>
-                <h2>Frequetly Asked Questions</h2>
-
-            </div>
+        <div className='faq section'>
+            <Title title='Frequently Asked Questions'/>
+            <div className='faq-container'>
+                <FaqItem question='Question 1' answer='Answer 1'/>
+                <FaqItem question='Question 2' answer='Answer 2'/>
+                <FaqItem question='Question 3' answer='Answer 3'/>
+                <FaqItem question='Question 4' answer='Answer 4'/>
+                <FaqItem question='Question 5' answer='Answer 5'/>
+            </div>        
+        </div>
 
         </>
     )
