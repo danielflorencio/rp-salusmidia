e1c44bbc8c09b88c986240de9cc2617427366c64
danielflorencio <danielflorencio20@gmail.com>
2022-11-17 00:07:00 -0300

    css animations now work with intersection observer

--- src/components/BigIconTitleDescriptionRow/BigIconTitleDescriptionRow.js
+++ src/components/BigIconTitleDescriptionRow/BigIconTitleDescriptionRow.js
@@ -6,69 +6,19 @@ import { useEffect, useRef, useState } from "react";
 
 export default function BigIconTitleDescriptionRow({img, subtitle, description, style, shadowyBorderBox}){
 
-    // const innerSection = useRef();
 
     // Documentation on how to do this: https://greensock.com/react/ 
     // Documentation on how to animate while using the intersection oberver api https://dev.to/producthackers/intersection-observer-using-react-49ko
     // https://medium.com/the-non-traditional-developer/how-to-use-an-intersectionobserver-in-a-react-hook-9fb061ac6cb5
     // https://www.npmjs.com/package/react-intersection-observer
     
-    // useLayoutEffect(() => {
-    //     let ctx = gsap.context(() => {
-    //         gsap.from('.inner-column', {duration: 0.8, x:'-10%', opacity: 0});
-    //     }, innerSection);
-    //     return () => ctx.revert();
-    // },[])
 
-    // const callBackFunction = (entries) => {
-    //     const callBackFunction = () => {
-    //     // const [entry] = entries
-    //     // setIsVisible(entry.isIntersecting)
-    //     useLayoutEffect(() => {
-    //         let ctx = gsap.context(() => {
-    //             gsap.from('.inner-column', {duration: 0.8, x:'-10%', opacity: 0});
-    //         }, ContainerRef);
-    //     return () => ctx.revert();
-    //     },[])
-    // }
-
-    
-    // const options = {
-    //     root: null,
-    //     rootMargin: '0px',
-    //     threshold: 1.0
-    // }
-
-    // const useElementOnScreen = (options) => {
-    //     const ContainerRef = useRef=(null)
-    //     const [isVisible, setIsVisible] = useState(false)
-    //     const callBackFunction = (entries) => {
-    //         const [entry] = entries
-    //         setIsVisible(entry.isIntersecting)
-    //     }
-
-    //     useEffect(() => {
-    //         const observer = new IntersectionObserver(callBackFunction, options)
-    //         if (ContainerRef.current) observer.observe(ContainerRef.current)
-    
-    //         return () => {
-    //             if (ContainerRef.current) observer.unobserve(ContainerRef.current)
-    //         }
-    //     }, [ContainerRef, options])
-    // }
-    
-    // const [ContainerRef, isVisible] = useElementOnScreen({
-    //     root: null,
-    //     rootMargin: '0px',
-    //     threshold: 1.0
-    // })
 
     const containerRef = useRef(null)
     const [isVisible, setIsVisible] = useState(false)
     
     const callBackFunction = (entries) => {
         const [entry] = entries
-        console.log('isIntersecting = ', entry.isIntersecting)
         setIsVisible(entry.isIntersecting)
     }
 
@@ -90,12 +40,12 @@ export default function BigIconTitleDescriptionRow({img, subtitle, description, 
     if (style === 'left-text-right-img'){
         return(
 
-            <section   className="inner-section fix-mobile-flex-direction">
+            <section className="inner-section fix-mobile-flex-direction">
                 <div ref={containerRef} class="inner-column left description-column">                
                     {isVisible ? (
                         <>
-                            <h2 className="text-to-left">{subtitle}</h2>
-                            <p>{description}</p>
+                            <h2 className="animate-column text-to-left">{subtitle}</h2>
+                            <p className="animate-column">{description}</p>
                         </>
                     ) : null}
                     {/* <h2 className="text-to-left">{subtitle}</h2>
@@ -127,21 +77,6 @@ export default function BigIconTitleDescriptionRow({img, subtitle, description, 
                     </div>
                 </div> */}
             </section>
-            // <section class="inner-section fix-mobile-flex-direction">
-                
-            //     {/* <div ref={ContainerRef} className="inner-column left description-column"> */}
-            //     <div ref={containerRef} class="inner-column left description-column">                
-            //         <h2 className="text-to-left">{subtitle}</h2>
-            //         <p>{description}</p>
-            //     </div>
-            //     <div class="inner-column right mockup img-column">
-            //         <div className={shadowyBorderBox ? 'shadowyBorderBox' : 'mockup-img'}>
-            //         <IconContext.Provider value={{ color: "#6ec1e4", className:'icon-sizer-125'}}>
-            //             {img}
-            //         </IconContext.Provider>
-            //         </div>
-            //     </div>
-            // </section>
         )
     } else if(style === 'right-text-left-img'){
         return(
@@ -154,7 +89,7 @@ export default function BigIconTitleDescriptionRow({img, subtitle, description, 
                     </div>
                 </div>
                 <div class="inner-column right description-column">
-                    <h2 className="text-to-left">{subtitle}</h2>
+                    <h2 className="animate-column text-to-left">{subtitle}</h2>
                     <p>{description}</p>
                 </div>
             </section>
