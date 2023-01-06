b3bc10983f921a27dae70092adc543539d84f0c7
danielflorencio <danielflorencio20@gmail.com>
2023-01-06 15:12:51 -0300

    refactored some code

--- src/components/BigIconTitleDescriptionRow/BigIconTitleDescriptionRow.js
+++ src/components/BigIconTitleDescriptionRow/BigIconTitleDescriptionRow.js
@@ -1,59 +1,84 @@
-import { IconContext } from "react-icons";
+    import { IconContext } from "react-icons";
 import { useIntersection } from "../../customHooks/useIntersection";
 import './styles.css';
 export default function BigIconTitleDescriptionRow({img, subtitle, description, style, shadowyBorderBox}){
     
     const [isIntersecting, ref] = useIntersection();
 
-    if (style === 'left-text-right-img'){
-        return(
-            <section ref={ref} className="inner-section fix-mobile-flex-direction">
-                <div className="inner-column left description-column">                
-                    {isIntersecting ? (
-                        <>
-                            <h2 className="animate-left-column text-to-left">{subtitle}</h2>
-                            <p className="animate-left-column">{description}</p>
-                        </>
-                    ) : null}                
-                </div>
+    return(
+        <section ref={ref} className={style + " inner-section fix-mobile-flex-direction"}>
+            <div className="inner-column left description-column">                
+                {isIntersecting ? (
+                    <>
+                        <h2 className="animate-left-column text-to-left">{subtitle}</h2>
+                        <p className="animate-left-column">{description}</p>
+                    </>
+                ) : null}                
+            </div>
 
-                <div className="inner-column right mockup img-column">
-                    {isIntersecting ? (
-                        <div className="animate-right-column">
-                            <IconContext.Provider value={{ color: "#6ec1e4", className:'icon-sizer-125'}}>
-                                {img}
-                            </IconContext.Provider>
-                        </div>
-                    ) : null}
-                </div>
-            </section>
-        )
-    } else if(style === 'right-text-left-img'){
-        return(
-            <section className="inner-section">
-                <div ref={ref} class="inner-column left mockup img-column">
-                    {isIntersecting ? (
-                        <div className="animate-left-column">
-                            <div className={shadowyBorderBox ? 'shadowyBorderBox' : 'mockup-img'}>
-                            <IconContext.Provider value={{ color: "#6ec1e4", className:'icon-sizer-125' }}>
-                                {img}
-                            </IconContext.Provider>
-                            </div>
-                        </div>
-                    ) : null}
-                </div>
-                <div class="inner-column right description-column">
-                    {isIntersecting ? (
-                        <div className="animate-right-column">
-                            <h2 className="text-to-left">{subtitle}</h2>
-                            <p>{description}</p>
-                        </div>
-                    ) : null}  
-                </div>
-            </section>
-        )
-    } else {
-        return(<h1>Error - Use 'left-text-right-img' or 'right-text-left-img' as the proper style attributes in the 'ImgTitleDescriptionSection' component.</h1>)
-    }
+            <div className="inner-column right mockup img-column">
+                {isIntersecting ? (
+                    <div className="animate-right-column">
+                        <IconContext.Provider value={{ color: "#6ec1e4", className:'icon-sizer-125'}}>
+                            {img}
+                        </IconContext.Provider>
+                    </div>
+                ) : null}
+            </div>
+        </section>
+    )
+
+
+
+    // if (style === 'left-text-right-img'){
+    //     return(
+    //         <section ref={ref} className="inner-section fix-mobile-flex-direction">
+    //             <div className="inner-column left description-column">                
+    //                 {isIntersecting ? (
+    //                     <>
+    //                         <h2 className="animate-left-column text-to-left">{subtitle}</h2>
+    //                         <p className="animate-left-column">{description}</p>
+    //                     </>
+    //                 ) : null}                
+    //             </div>
+
+    //             <div className="inner-column right mockup img-column">
+    //                 {isIntersecting ? (
+    //                     <div className="animate-right-column">
+    //                         <IconContext.Provider value={{ color: "#6ec1e4", className:'icon-sizer-125'}}>
+    //                             {img}
+    //                         </IconContext.Provider>
+    //                     </div>
+    //                 ) : null}
+    //             </div>
+    //         </section>
+    //     )
+    // } else if(style === 'right-text-left-img'){
+    //     return(
+    //         <section className="inner-section">
+    //             <div ref={ref} class="inner-column left mockup img-column">
+    //                 {isIntersecting ? (
+    //                     <div className="animate-left-column">
+    //                         <div className={shadowyBorderBox ? 'shadowyBorderBox' : 'mockup-img'}>
+    //                         <IconContext.Provider value={{ color: "#6ec1e4", className:'icon-sizer-125' }}>
+    //                             {img}
+    //                         </IconContext.Provider>
+    //                         </div>
+    //                     </div>
+    //                 ) : null}
+    //             </div>
+    //             <div class="inner-column right description-column">
+    //                 {isIntersecting ? (
+    //                     <div className="animate-right-column">
+    //                         <h2 className="text-to-left">{subtitle}</h2>
+    //                         <p>{description}</p>
+    //                     </div>
+    //                 ) : null}  
+    //             </div>
+    //         </section>
+    //     )
+    // } else {
+    //     return(<h1>Error - Use 'left-text-right-img' or 'right-text-left-img' as the proper style attributes in the 'ImgTitleDescriptionSection' component.</h1>)
+    // }
 
 }
--- src/components/ServicesSection/ServicesSection.js
+++ src/components/ServicesSection/ServicesSection.js
@@ -17,28 +17,28 @@ export default function ServicesSection(){
             img={<BsGoogle/>} 
             subtitle="Google Ads" 
             description="Have you ever wondered why the patient went to your competitor and not you? With the power of Google Ads, you will be found by up to 3x more patients! Be the one who is ahead of the competition." 
-            style="left-text-right-img" 
+            style="default" 
             shadowyBorderBox={false}/>
             
             <BigIconTitleDescriptionRow
             img={<CgWebsite/>} 
             subtitle="Attractive and persuasive website" 
             description="We will create the perfect website to make your customer delighted with your service. With this, your patient will get to your attendant more prepared to schedule for a private consultation." 
-            style="right-text-left-img" 
+            style="reverse" 
             shadowyBorderBox={false}/>
             
             <BigIconTitleDescriptionRow
             img={<AiOutlineLineChart/>}
             subtitle="Metric analysis and improving" 
             description="Long term is the key! In addition to bringing you many new patients from the first month onwards, with our quarterly analysis of metrics and structure, the trend is that your result will always increase each month." 
-            style="left-text-right-img" 
+            style="default" 
             shadowyBorderBox={false}/> 
             
             <BigIconTitleDescriptionRow
             img={<AiFillFacebook/>} 
             subtitle="Facebook and Instagram ads" 
             description="Keep actively selling your best procedures to the world’s most used social networks. With Facebook and Instagram working for you, getting your treatments and procedures to reach more people will be easier than taking a nap in a rainy afternoon." 
-            style="right-text-left-img" 
+            style="reverse" 
             shadowyBorderBox={false}/>   
         </section>
     )
