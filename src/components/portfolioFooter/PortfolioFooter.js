8c0bf338448b2a4095fc8de69f344296a5b81a4f
danielflorencio <danielflorencio20@gmail.com>
2023-01-24 14:06:07 -0300

    fixed the link on the portfolio footer

--- src/components/portfolioFooter/PortfolioFooter.js
+++ src/components/portfolioFooter/PortfolioFooter.js
@@ -2,7 +2,7 @@ import './styles.css'
 export default function PortfolioFooter(){
     return(
         <footer className='footer'>
-            <strong>Hey, nice to meet you, my name is Daniel 😃 and this is one of my portfolio projects. <a href="https://next-portfolio-pearl-five.vercel.app/">Click here</a> to know more about my work.</strong>
+            <strong>Hey, nice to meet you, my name is Daniel 😃 and this is one of my portfolio projects. <a href="https://next-portfolio-pearl-five.vercel.app/" target="_blank" rel="noopener noreferrer">Click here</a> to know more about my work.</strong>
         </footer>
     )
 }
