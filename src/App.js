73c9582e893b3c93e82e0f1f4d96247cc44d81c3
danielflorencio <danielflorencio20@gmail.com>
2023-01-24 14:03:00 -0300

    added my portfolio footer

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ import StepByStepSection from './components/StepByStepSection/StepByStepSection'
 import CtaSection from './components/CtaSection/CtaSection';
 import FaqSection from './components/FaqSection/FaqSection';
 import Footer from './components/Footer/Footer';
-
+import PortfolioFooter from './components/portfolioFooter/PortfolioFooter';
 export default function App() {
   return (
     <>
-      {/* <HeroSection /> */}
+      <HeroSection />
       <ServicesSection />
       <IconsAndDescriptionSection/>
       <StepByStepSection/>
       <CtaSection/>
       <FaqSection/>
       <Footer/>
+      <PortfolioFooter/>
     </>
     );
 }
---
+++ src/components/portfolioFooter/PortfolioFooter.js
@@ -0,0 +1,8 @@
+import './styles.css'
+export default function PortfolioFooter(){
+    return(
+        <footer className='footer'>
+            <strong>Hey, nice to meet you, my name is Daniel 😃 and this is one of my portfolio projects. <a href="https://next-portfolio-pearl-five.vercel.app/">Click here</a> to know more about my work.</strong>
+        </footer>
+    )
+}
